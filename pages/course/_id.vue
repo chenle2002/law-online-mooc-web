df37d4e4efb6a69f48d8bf165b4e9006ba784b5e
chenle2002 <chenle2020@mail.ynu.edu.cn>
2023-05-21 16:26:48 +0800

    页面补充

--- pages/course/_id.vue
+++ pages/course/_id.vue
@@ -12,7 +12,7 @@
       <div>
         <article class="c-v-pic-wrap" style="height: 357px;">
           <section class="p-h-video-box" id="videoPlay">
-            <img :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
+            <img height="357px" :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
           </section>
         </article>
         <aside class="c-attr-wrap">
@@ -33,13 +33,16 @@
                 <a class="c-fff vam" title="收藏" href="#" >收藏</a>
               </span>
             </section>
-            <section class="c-attr-mt">
+            <section  v-if="isbuy || Number(courseWebVo.price) === 0" class="c-attr-mt">
               <a href="#" title="立即观看" class="comm-btn c-btn-3">立即观看</a>
+            </section>
+            <section  v-else class="c-attr-mt">
+              <a @click="createOrders()" href="#" title="立即购买" class="comm-btn c-btn-3">立即购买</a>
             </section>
           </section>
         </aside>
         <aside class="thr-attr-box">
-          <ol class="thr-attr-ol clearfix">
+          <ol class="thr-attr-ol">
             <li>
               <p>&nbsp;</p>
               <aside>
@@ -162,15 +165,40 @@
 
 <script>
 import courseApi from '@/api/course'
+import ordersApi from '@/api/orders'
 export default {
   asyncData({ params, error }) {
-    return courseApi.getCourseInfo(params.id)
-      .then(response => {
-        return {
-          courseWebVo: response.data.data.courseWebVo,
-          chapterVideoList: response.data.data.chapterVideoList
-        }
-      })
+    return {courseId: params.id}
+  },
+  data() {
+    return {
+      courseWebVo: {},
+      chapterVideoList: [],
+      isbuy: false,
+    }
+  },
+  created() {//在页面渲染之前执行
+    this.initCourseInfo()
+  },
+  methods:{
+    //查询课程详情信息
+    initCourseInfo() {
+      courseApi.getCourseInfo(this.courseId)
+        .then(response => {
+          this.courseWebVo=response.data.data.courseWebVo,
+            this.chapterVideoList=response.data.data.chapterVideoList,
+            this.isbuy=response.data.data.isBuy
+        })
+    },
+    //生成订单
+    createOrders() {
+      ordersApi.createOrders(this.courseId)
+        .then(response => {
+          //获取返回订单号
+          //生成订单之后，跳转订单显示页面
+          this.$router.push({path:'/orders/'+response.data.data.orderId})
+        })
+    }
   }
 };
 </script>
